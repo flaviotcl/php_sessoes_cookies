c3edd12145d615ab8c1023480058a0bb77cdd34b
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-20 18:07:54 -0300

    Criando Sessoes + Manipulando os dados da Secao

--- outra-pagina/index.php
+++ outra-pagina/index.php
@@ -1,4 +1,13 @@
 <?php
+/*** Trabalhando com Cookies ***/
+//var_dump($_COOKIE);
 
 
-var_dump($_COOKIE);
+/*** Criando Sessões ***/
+//session_save_path(__DIR__.'/../sessions');
+
+
+ /***  Manipulando os dados da Sessão  ***/
+ session_start();
+
+ var_dump($_SESSION);
