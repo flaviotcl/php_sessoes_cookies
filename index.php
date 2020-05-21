7b59e804a6aa723fcf9bcc93b876b6d470f916d8
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-21 12:11:41 -0300

    Exemplo prático Login Básico

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 
+session_start();
+
+$user = $_SESSION['user'] ?? null;
+
+if(!$user){
+    header('location: login.php');
+    exit;
+}
+
+?>
+
+<h1>Página Protegida</h1>
+
+<p>Olá, <?php echo $user['email']; ?></p>
